a988e6873540c8fb37c59d27c1aeb25bf39192ea
Marko Mirtic <mmirtic@apple.com>
2023-09-12 13:08:30 +0200

    Portfolio Development

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,7 +156,7 @@ themeButton.addEventListener('click', () =>{
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
+    duration: 1500,
     delay: 400,
     reset: true //Animations repeat 
 })
